7c9f9000f40399a5a5c6292b121da8b651830aec
jorttp <joserpo2003@gmail.com>
2026-05-28 16:27:44 -0500

    configuracion

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
-import * as test from "node:test";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test('renders app', () => {
-  expect(true).toBe(true);
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
